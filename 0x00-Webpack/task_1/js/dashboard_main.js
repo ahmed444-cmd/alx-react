97783ec9fc386cb87b67a13360fce512b98d13ad
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-11-04 03:56:52 -0800

    Mandatory

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+
+$(document).ready(function() {
+  // Create and append elements
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button id="clickButton">Click here to get started</button>');
+  $('body').append('<p id="count">0 clicks on the button</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+  function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  // Bind the debounce function to the click event
+  const debouncedUpdateCounter = debounce(updateCounter, 300);
+  $('#clickButton').on('click', debouncedUpdateCounter);
+});
