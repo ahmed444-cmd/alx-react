abb2aaf632d2382d72adb2bd82e9182276b78bcd
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-11-05 01:56:39 -0800

    Mandatory

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  mode: 'production',
   module: {
     rules: [
       {
@@ -14,20 +14,12 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[hash][ext][query]'
-        },
+        test: /\.(png|jpg|gif|svg)$/,
         use: [
           {
-            loader: 'image-webpack-loader',
+            loader: 'file-loader',
             options: {
-              mozjpeg: { progressive: true, quality: 65 },
-              optipng: { enabled: false },
-              pngquant: { quality: [0.65, 0.90], speed: 4 },
-              gifsicle: { interlaced: false },
-              webp: { quality: 75 },
+              name: '[path][name].[ext]',
             },
           },
         ],
