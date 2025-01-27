bc5068e3674a34c9627ea48338a2700bab05e652
El Abid Ahmed <ahmedelabide@gmail.com>
2025-01-28 00:24:47 +0100

    task1

--- 0x01-react_intro/task_0/dashboard/app.js
+++ 0x01-react_intro/task_0/dashboard/app.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import './App.css'; // Ensure you have some styles for these classes
+import './App.css';  // Make sure App.css exists
 
 function App() {
   return (
     <div className="App">
-      {/* Header Section */}
       <div className="App-header">
-        <img src="./holberton-logo.png" alt="ALX Logo" />
+        <img src="path_to_alx_logo.png" alt="ALX Logo" />
         <h1>School dashboard</h1>
       </div>
 
-      {/* Body Section */}
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
 
-      {/* Footer Section */}
       <div className="App-footer">
         <p>Copyright 2020 - ALX</p>
       </div>
