cc0f72866b108a66b6ed6bb33fe3963dce3f548e
El Abid Ahmed <ahmedelabide@gmail.com>
2025-01-28 00:55:35 +0100

    task1

---
+++ 0x01-react_intro/task_1/dashboard/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
+
+// Create root for notifications
+const rootNotifications = ReactDOM.createRoot(document.getElementById('root-notifications'));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
+
+// Create root for main app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
